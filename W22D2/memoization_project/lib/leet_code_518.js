5b9249eb0eee9c4aff299084ad55084aff35e13f
Albert Cheng <lptalbert@gmail.com>
2019-11-26 23:01:10 -0800

    complete leet code coin change 2

--- W22D2/memoization_project/lib/leet_code_518.js
+++ W22D2/memoization_project/lib/leet_code_518.js
@@ -1,3 +1,16 @@
 // Work through this problem on https://leetcode.com/problems/coin-change-2/ and use the specs given there.
 // Feel free to use this file for scratch work.
 
+function change(amount, coins, memo = {}) {
+  const key = amount + "-" + coins;
+  if (key in memo) return memo[key];
+  if (amount === 0) return 1;
+
+  let ways = 0;
+  for (let i = 0; i * coins[coins.length - 1] <= amount; i++) {
+    ways += change(amount - i * coins[coins.length - 1], coins.slice(0, coins.length - 1), memo);
+  }
+
+  memo[key] = ways;
+  return memo[key];
+}
