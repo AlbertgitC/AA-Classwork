5f5eeb39377e3e8f4e5420cfd2a6505fe64070c5
Albert Cheng <lptalbert@gmail.com>
2019-12-02 08:46:48 -0800

    complete binarySearch

--- W23D1/binary_search_project/lib/binary_search.js
+++ W23D1/binary_search_project/lib/binary_search.js
@@ -1,5 +1,19 @@
 function binarySearch(array, target) {
+    if (array.length === 0) {
+        return false;
+    }
 
+    const middle = Math.floor(array.length / 2);
+    const left = array.slice(0, middle);
+    const right = array.slice(middle + 1);
+
+    if (target < array[middle]) {
+        return binarySearch(left, target);
+    } else if (target > array[middle]) {
+        return binarySearch(right, target);
+    } else {
+        return true;
+    }
 }
 
 function binarySearchIndex(array, target) {
