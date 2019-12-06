420be59966495ee289b9f2903b97b12c8958d197
Albert Cheng <lptalbert@gmail.com>
2019-12-05 17:10:08 -0800

    complete leet code 105 for W23D3

--- W23D3/tree_order_project/lib/leet_code_105.js
+++ W23D3/tree_order_project/lib/leet_code_105.js
@@ -5,5 +5,20 @@ const { TreeNode } = require('./tree_node.js');
 
 
 function buildTree(preorder, inorder) {
-
+  if (preorder.length === 0) return null;
+  const root = preorder[0];
+  const root_idx = inorder.indexOf(root);
+  
+  const left = inorder.slice(0, root_idx)
+  const right = inorder.slice(root_idx + 1);
+  const pre_left = [];
+  const pre_right = [];
+  preorder.forEach(ele => {
+    if (left.includes(ele)) pre_left.push(ele);
+    if (right.includes(ele)) pre_right.push(ele);
+  });
+  const root_node = new TreeNode(root);
+  root_node.left = buildTree(pre_left, left);
+  root_node.right = buildTree(pre_right, right);
+  return root_node;
 }
