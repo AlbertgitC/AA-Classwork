72b96142433f42bc87794ea263599ad3cb24522e
Albert Cheng <lptalbert@gmail.com>
2019-12-06 15:41:36 -0800

    complete numRegions for W23D5

--- W23D5/graph_project/lib/num_regions.js
+++ W23D5/graph_project/lib/num_regions.js
@@ -1,5 +1,25 @@
 function numRegions(graph) {
-
+    let visited = new Set();
+    let counter = 0;
+    for (let node in graph) {
+        if (!visited.has(node)) {
+            visited.add(node);
+            const queue = [graph[node]];
+            while (queue.length) {
+                let neighbors = queue.shift();
+                
+                neighbors.forEach(node => {
+                    if (!visited.has(node)) {
+                        visited.add(node);
+                        queue.push(graph[node]);
+                    }
+                });
+            }
+            counter += 1;
+        }
+    }
+    
+    return counter;
 }
 
 module.exports = {
