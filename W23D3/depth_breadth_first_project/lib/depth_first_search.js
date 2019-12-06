67790d2db36b1a1bba27d8fd3def3aea2662de1b
Albert Cheng <lptalbert@gmail.com>
2019-12-05 17:24:39 -0800

    complete depth first search for W23D3

--- W23D3/depth_breadth_first_project/lib/depth_first_search.js
+++ W23D3/depth_breadth_first_project/lib/depth_first_search.js
@@ -1,5 +1,9 @@
 function depthFirstSearch(root, targetVal) {
-
+    if (!root) return null;
+    if (root.val === targetVal) return root;
+    
+    if (depthFirstSearch(root.left, targetVal)) return depthFirstSearch(root.left, targetVal);
+    return depthFirstSearch(root.right, targetVal);
 }
 
 
