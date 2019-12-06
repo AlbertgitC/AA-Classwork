9330742260243d9082b4a83f8ed02cc5987c1b02
Albert Cheng <lptalbert@gmail.com>
2019-12-06 15:13:04 -0800

    complete max value for W23D5

--- W23D5/graph_project/lib/max_value.js
+++ W23D5/graph_project/lib/max_value.js
@@ -1,5 +1,25 @@
 function maxValue(node, visited=new Set()) {
-
+    let max_val = node.val;
+    const queue = [node];
+    while (queue.length) {
+        let current_node = queue.shift();
+        visited.add(current_node);
+        if (current_node.val > max_val) {
+            max_val = current_node.val;
+            current_node.neighbors.forEach(node => {
+                if (!visited.has(node)) {
+                    queue.push(node);
+                }
+            });
+        } else {
+            current_node.neighbors.forEach(node => {
+                if (!visited.has(node)) {
+                    queue.push(node);
+                }
+            });
+        }
+    }
+    return max_val;
 }
 
 module.exports = {
