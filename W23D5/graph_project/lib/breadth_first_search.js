3da8da632af99449e04093b24452134da57ebc82
Albert Cheng <lptalbert@gmail.com>
2019-12-06 17:47:46 -0800

    complete leet code 207 for W23D5

--- W23D5/graph_project/lib/breadth_first_search.js
+++ W23D5/graph_project/lib/breadth_first_search.js
@@ -1,20 +1,34 @@
 
 
 function breadthFirstSearch(startingNode, targetVal) {
+    // let visited = new Set();
+    // const queue = [startingNode];
+    // while (queue.length) {
+    //     let current_node = queue.shift();
+    //     visited.add(current_node);
+    //     if (current_node.val === targetVal) {
+    //         return current_node;
+    //     } else {
+    //         current_node.neighbors.forEach(node => {
+    //             if (!visited.has(node)) {
+    //                 queue.push(node);
+    //             }
+    //         });
+    //     }
+    // }
+    // return null;
+
     let visited = new Set();
     const queue = [startingNode];
+
     while (queue.length) {
         let current_node = queue.shift();
+        if (visited.has(current_node)) continue;
+
         visited.add(current_node);
-        if (current_node.val === targetVal) {
-            return current_node;
-        } else {
-            current_node.neighbors.forEach(node => {
-                if (!visited.has(node)) {
-                    queue.push(node);
-                }
-            });
-        }
+        if (current_node.val === targetVal) return current_node;
+
+        queue.push(...current_node.neighbors);
     }
     return null;
 }
--- W23D5/graph_project/lib/leet_code_207.js
+++ W23D5/graph_project/lib/leet_code_207.js
@@ -1,6 +1,41 @@
 // View the full problem and run the test cases at:
 //  https://leetcode.com/problems/course-schedule/
 
-function canFinish(numCourses, prerequisites) {
+function buildGraph(list) {
+  const graph = {};
+  list.forEach(prereq => {
+    const [course, pre] = prereq.map(String);
+    if (course in graph) {
+      graph[course].push(pre);
+    } else {
+      graph[course] = [pre];
+    }
 
+    if (!(pre in graph)) {
+      graph[pre] = [];
+    }
+  });
+
+  return graph;
+}
+
+function canFinish(numCourses, prerequisites) {
+  const graph = buildGraph(prerequisites);
+  const completed = new Set();
+  const total_courses = Object.keys(graph).length;
+
+  let eligible = true;
+  while (eligible) {
+    eligible = false;
+
+    for (let course in graph) {
+      const prereq_met = graph[course].every(prereq => completed.has(prereq));
+      if (!completed.has(course) && prereq_met) {
+        completed.add(course);
+        eligible = true;
+      }
+    }
+  }
+
+  return completed.size === total_courses;
 }
