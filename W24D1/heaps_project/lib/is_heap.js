ef926dffbc4dd319a8f39582204fab97c40b9f6b
Albert Cheng <lptalbert@gmail.com>
2019-12-18 17:43:18 -0800

    complete is_heap for W24D1

--- W24D1/heaps_project/lib/is_heap.js
+++ W24D1/heaps_project/lib/is_heap.js
@@ -1,6 +1,17 @@
 // you may assume that the array will always have a null element at the 0-th index
 function isMaxHeap(array, idx=1) {
+    const left_idx = idx * 2;
+    const right_idx = idx * 2 + 1;
+    let left_val = array[left_idx];
+    let right_val = array[right_idx];
+    if (right_val === undefined) right_val = -Infinity;
+    if (left_val === undefined) return true;
+    if (array[idx] > left_val && array[idx] > right_val) {
+        if (isMaxHeap(array, left_idx) && isMaxHeap(array, right_idx)) return true;
+        return false;
+    }
 
+    return false;
 }
 
 
