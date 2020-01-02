c1e294a63a115c5a28a7f434bdf5c6aaf81cc9fd
Albert Cheng <lptalbert@gmail.com>
2020-01-01 18:44:12 -0800

    complete insertRecur for W24D2

--- W24D2/trie_project/lib/trie.js
+++ W24D2/trie_project/lib/trie.js
@@ -6,7 +6,24 @@ class Node {
 }
 
 class Trie {
-   
+    constructor() {
+        this.root = new Node();
+    }
+
+    insertRecur(word, root = this.root) {
+        const first_char = word[0];
+
+        if (!(first_char in root.children)) {
+            root.children[first_char] = new Node();
+        }
+
+        if (word.length === 1) {
+            root.children[first_char].isTerminal = true;
+        } else {
+            this.insertRecur(word.slice(1), root.children[first_char]);
+        }
+        
+    }
 }
 
 module.exports = {
